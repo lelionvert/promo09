ca79409f5431482e7cbba9062996439cb63c7087
CitizenFiftyTwo <fabien.labrousse@la-combe-du-lion-vert.fr>
2019-04-12 14:13:06 +0200

    zero participant case

---
+++ SocratesFranceTest/ColdMealTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace SocratesFranceTest
+{
+    [TestClass]
+    public class ColdMealTest
+    {
+        [TestMethod]
+        public void ZeroDateReturnZeroParticipant()
+        {
+            int participantNumber = ColdMeals.GetParticipantNumber();
+
+            int expected = 0;
+            participantNumber.Should().Be(expected);
+
+
+        }
+
+
+
+    }
+}
---
+++ SocratesFranceTest/ColdMeals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocratesFranceTest
+{
+    internal class ColdMeals
+    {
+        internal static int GetParticipantNumber()
+        {
+            return 0;
+        }
+    }
+}
