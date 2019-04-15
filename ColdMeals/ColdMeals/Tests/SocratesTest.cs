36a47f612ff46cc397941d4c3eff23f7341b1412
Sanwan <erwan.duhildebenaze@lacombedulionvert.fr>
2019-04-15 16:06:08 +0200

    refactor checkins

--- ColdMeals/ColdMeals/Tests/SocratesTest.cs
+++ ColdMeals/ColdMeals/Tests/SocratesTest.cs
@@ -7,46 +7,32 @@ namespace Tests
 {
     public class SocratesTest
     {
-
-        [Test]
-        public void Checkin_list_is_not_null()
-        {
-            Socrates socrates = new Socrates(new DateTime(2019, 11, 18, 21, 00, 00));
-            socrates.Checkins.Should().NotBeNull();
-        }
-
-        [Test]
-        public void Add_checkin_to_the_checkin_list_return_not_null_or_empty_list()
-        {
-            Socrates socrates = new Socrates(new DateTime(2019, 11, 18, 21, 00, 00));
-
-            socrates.AddCheckin(new DateTime(2019, 11, 18, 18, 00, 00));
-
-            socrates.Checkins.Should().NotBeNullOrEmpty();
-        }
-
         [Test]
         public void Count_number_of_cold_meals_return_0()
         {
-            Socrates socrates = new Socrates(new DateTime(2019, 11, 18), new DateTime(2019, 11, 18, 21, 00, 00));
+            var startedDate = new DateTime(2019, 11, 18);
+            var coldMealStartDate = new DateTime(2019, 11, 18, 21, 00, 00);
+            Checkins socrates = new Checkins();
 
             socrates.AddCheckin(null);
             socrates.AddCheckin(new DateTime(2019, 11, 18, 20, 00, 00));
             socrates.AddCheckin(new DateTime(2019, 11, 19, 22, 00, 00));
 
-            int result = socrates.CountNumberOfColdMeals();
+            int result = socrates.CountBetween(coldMealStartDate, startedDate.Date.AddDays(1));
 
             result.Should().Be(0);
         }
         [Test]
         public void Count_number_of_cold_meals_return_1()
         {
-            Socrates socrates = new Socrates(new DateTime(2019, 11, 18), new DateTime(2019, 11, 18, 21, 00, 00));
+            var startedDate = new DateTime(2019, 11, 18);
+            var coldMealStartDate = new DateTime(2019, 11, 18, 21, 00, 00);
+            Checkins socrates = new Checkins();
             socrates.AddCheckin(new DateTime(2019, 11, 19, 00, 00, 00));
             socrates.AddCheckin(new DateTime(2019, 11, 18, 20, 00, 00));
             socrates.AddCheckin(new DateTime(2019, 11, 18, 22, 00, 00));
 
-            int result = socrates.CountNumberOfColdMeals();
+            int result = socrates.CountBetween(coldMealStartDate, startedDate.Date.AddDays(1));
 
             result.Should().Be(1);
         }
@@ -54,12 +40,14 @@ namespace Tests
         [Test]
         public void Count_number_of_cold_meals_return_2()
         {
-            Socrates socrates = new Socrates(new DateTime(2019, 11, 18), new DateTime(2019, 11, 18, 21, 00, 00));
+            var startedDate = new DateTime(2019, 11, 18);
+            var coldMealStartDate = new DateTime(2019, 11, 18, 21, 00, 00);
+            Checkins socrates = new Checkins();
             socrates.AddCheckin(new DateTime(2019, 11, 18, 23, 00, 00));
             socrates.AddCheckin(new DateTime(2019, 11, 18, 23, 59, 00));
             socrates.AddCheckin(new DateTime(2019, 11, 18, 10, 00, 00));
 
-            int result = socrates.CountNumberOfColdMeals();
+            int result = socrates.CountBetween(coldMealStartDate, startedDate.Date.AddDays(1));
 
             result.Should().Be(2);
         }
---
+++ ColdMeals/Core/Checkins.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ColdMeals.Core
+{
+    public class Checkins
+    {
+        private readonly List<Checkin> _checkins;
+
+        public Checkins()
+        {
+            _checkins = new List<Checkin>();
+        }
+
+        public void AddCheckin(DateTime? arrivedDate)
+        {
+            Checkin checkin = Checkin.CreateCheckin(arrivedDate);
+            _checkins.Add(checkin);
+        }
+        public int CountBetween(DateTime startDate, DateTime endDate)
+        {
+            return _checkins.Where(checkin => checkin.IsArrivingBetween(startDate, endDate))
+                           .Count();
+        }
+    }
+}
