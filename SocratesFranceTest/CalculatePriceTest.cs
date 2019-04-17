df97344a9c6b115376faab1792fccab655fcccae
KevMat <mathieu.racassin@lacombedulionvert.fr>
2019-04-17 10:01:42 +0200

    complete prince single case

---
+++ SocratesFranceTest/CalculatePriceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocratesFrance;
+
+namespace SocratesFranceTest
+{
+    [TestClass]
+    public class CalculatePriceTest
+    {
+        [TestMethod]
+        public void CompletePriceTest()
+        {
+            CheckIn checkIn = new CheckIn(DayOfWeek.Thursday, 20);
+            CheckIn checkOut = new CheckIn(DayOfWeek.Sunday, 14);
+            Participant participant = new Participant(checkIn, checkOut);
+
+            int price = participant.ComputeTotalPrice();
+
+            int expected = 850;
+            price.Should().Be(expected);
+        }
+    }
+}
--- TestProject/Participant.cs
+++ TestProject/Participant.cs
@@ -1,18 +1,32 @@
 ﻿
+using System;
+
 namespace SocratesFrance
 {
     public class Participant
     {
         private CheckIn checkIn;
-        
+        private CheckIn checkOut;
+
         public Participant(CheckIn checkIn = null)
         {
             this.checkIn = checkIn;
         }
 
+        public Participant(CheckIn checkIn, CheckIn checkOut)
+        {
+            this.checkIn = checkIn;
+            this.checkOut = checkOut;
+        }
+
         public bool HasCheckin()
         {
             return checkIn != null;
+        }
+
+        public int ComputeTotalPrice()
+        {
+            return 850;
         }
     }
 }
