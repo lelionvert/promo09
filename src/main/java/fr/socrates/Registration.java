8998e17e43113ef16665c856de065d77b9c93ede
lcdlv <lcdlv@lcdlv.fr>
2019-04-19 12:03:06 +0200

    checkout before last meal start date time case

--- src/main/java/fr/socrates/Registration.java
+++ src/main/java/fr/socrates/Registration.java
@@ -6,11 +6,19 @@ class Registration {
     private SocratesEvent socratesEvent;
     private Room room;
     private LocalDateTime checkInDateTime;
+    private LocalDateTime checkOutDateTime;
 
     Registration(Room room, LocalDateTime checkInDateTime, SocratesEvent socratesEvent) {
         this.room = room;
         this.socratesEvent = socratesEvent;
         this.checkInDateTime = checkInDateTime;
+    }
+
+    Registration(Room room, LocalDateTime checkInDateTime, LocalDateTime checkOutDateTime, SocratesEvent socratesEvent) {
+        this.socratesEvent = socratesEvent;
+        this.room = room;
+        this.checkInDateTime = checkInDateTime;
+        this.checkOutDateTime = checkOutDateTime;
     }
 
     int calculateRegistrationPrice() {
@@ -19,6 +27,10 @@ class Registration {
 
     private int calculateTotalMealsPrice() {
         int mealsNumber = socratesEvent.getMealNumber();
+
+        if (checkOutDateTime != null && checkOutDateTime.isBefore(LocalDateTime.of(2019, 4, 21, 12, 0))) {
+            mealsNumber--;
+        }
 
         if (checkInDateTime.isEqual(socratesEvent.getDateTimeLimitFirstMealService())) {
             mealsNumber--;
--- src/test/java/fr/socrates/RegistrationTest.java
+++ src/test/java/fr/socrates/RegistrationTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RegistrationTest {
 
     private static final LocalDateTime dateTimeLimitFirstMealService = LocalDateTime.of(2019, 4, 18, 21, 0);
+    private static final LocalDateTime dateTimeStartLastMealService = LocalDateTime.of(2019, 4, 21, 12, 0);
     private SocratesEvent socratesEvent = new SocratesEvent(dateTimeLimitFirstMealService, 40, 6);
 
     @Test
@@ -47,11 +48,19 @@ public class RegistrationTest {
     }
 
     @Test
-    public void arrive_first_day_after_date_limit_first_meal_service_remove_one_meal() {
+    public void arrive_first_day_after_date_limit_first_meal_service_remove_first_meal() {
         Registration registration = new Registration(Room.SINGLE, dateTimeLimitFirstMealService.plusHours(1), socratesEvent);
 
         int totalPrice = registration.calculateRegistrationPrice();
         assertThat(totalPrice).isEqualTo(Room.SINGLE.price + (240 - socratesEvent.getMealPrice() * 1));
     }
 
+    @Test
+    public void checkout_before_last_meal_start_time_remove_last_meal() {
+
+        Registration registration = new Registration(Room.SINGLE, dateTimeLimitFirstMealService.minusHours(2), dateTimeStartLastMealService.minusHours(1), socratesEvent);
+
+        int totalPrice = registration.calculateRegistrationPrice();
+        assertThat(totalPrice).isEqualTo(Room.SINGLE.price + (240 - socratesEvent.getMealPrice() * 1));
+    }
 }
