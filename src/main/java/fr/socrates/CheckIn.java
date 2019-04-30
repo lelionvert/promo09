88c130cfc7d7a47c1bcd67f86b572e7b5fefa40f
lcdlv <lcdlv@lcdlv.fr>
2019-04-30 09:44:08 +0200

    refactor remove isAfter() in class Checkein

--- src/main/java/fr/socrates/CheckIn.java
+++ src/main/java/fr/socrates/CheckIn.java
@@ -22,11 +22,6 @@ class CheckIn {
                 && actualCheckInDateTime.isAfter(latenessDeadLine);
     }
 
-    boolean isAfter(LocalDateTime date) {
-        LocalDateTime actualCheckInDateTime = checkInDateTime.orElse(LocalDateTime.MIN);
-        return actualCheckInDateTime.isAfter(date);
-    }
-
     @Override
     public String toString() {
         return "CheckIn{" +
